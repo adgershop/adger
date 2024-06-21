62a20337b9ba65eef94b697159780498b47af7ea
adgershop <saif.adger.1@gmail.com>
2024-06-21 13:04:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function openGmail() {
     var body = "اسم الشخص: " + name + "\nرقم الهاتف: " + phoneNumber + "\n"; // Body text
 
     // Construct Gmail URL with parameters
-    var gmail_url = 'https://mail.google.com/mail/u/0/?fs=1&to=' + encodeURIComponent(email) + '&su=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-    
+    var gmail_url = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=' + encodeURIComponent(email) + '&su=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+
     // Open Gmail in a new tab
     window.open(gmail_url, '_blank');
 }
